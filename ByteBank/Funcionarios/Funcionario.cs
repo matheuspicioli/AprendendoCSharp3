e82bf93b6a3ec52c070dcbb2b638f969bd5c06e6
Matheus Picioli <matheus.picioli98@gmail.com>
2018-12-11 21:45:30 -0200

    Namespace Funcionarios e classe Funcionario

---
+++ ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+
+        public Funcionario(int tipo)
+        {
+            this._tipo = tipo;
+        }
+
+        public double GetBonificacao(int tipo)
+        {
+            if (_tipo == 1)
+                return this.Salario;
+
+            return this.Salario * 0.10;
+        }
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Funcionarios;
+using System;
 
 namespace ByteBank
 {
@@ -6,7 +7,15 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Funcionario carlos = new Funcionario(1);
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
         }
     }
 }
